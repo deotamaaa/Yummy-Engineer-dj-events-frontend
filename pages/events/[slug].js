a749f1585cf00327091e4a9dd2955257b686f6b7
Deotama <deotama11@gmail.com>
2022-03-17 16:59:08 +0700

    Next 3.4

--- pages/events/[slug].js
+++ pages/events/[slug].js
@@ -1,14 +1,38 @@
 import { useRouter } from 'next/router';
 import React from 'react';
 import Layout from '@/components/Layout';
+import { API_URL } from '@/config/index';
 
-export default function EventPage() {
+export default function EventPage({ evt }) {
   const router = useRouter();
 
   return (
     <Layout>
-      <h1>EventPage</h1>
-      <h3>{router.query.slug}</h3>
+      <h1>{evt.name}</h1>
     </Layout>
   );
+}
+
+export async function getStaticPaths() {
+  const res = await fetch(`${API_URL}/api/events`);
+  const events = await res.json();
+  const paths = events.map((evt) => ({
+    params: { slug: evt.slug },
+  }));
+
+  return {
+    paths,
+    fallback: true,
+  };
+}
+
+export async function getStaticProps({ params: { slug } }) {
+  const res = await fetch(`${API_URL}/api/events/${slug}`);
+  const events = await res.json();
+  return {
+    props: {
+      evt: events[0],
+    },
+    revalidate: 1,
+  };
 }
