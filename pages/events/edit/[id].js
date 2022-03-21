ad392d015afbc4f6245476ca830ad50243aa37e1
Deotama <deotama11@gmail.com>
2022-03-21 11:44:56 +0700

    Next 6.4

--- pages/events/edit/[id].js
+++ pages/events/edit/[id].js
@@ -10,6 +10,7 @@ import Modal from '@/components/Modal';
 import { API_URL } from '@/config/index';
 import styles from '@/styles/Form.module.css';
 import Image from 'next/image';
+import ImageUpload from '@/components/ImageUpload';
 
 export default function EditEventPage({ evt }) {
   const [values, setValues] = useState({
@@ -63,6 +64,13 @@ export default function EditEventPage({ evt }) {
       ...values,
       [name]: value,
     });
+  };
+
+  const imageUploaded = async (e) => {
+    const res = await fetch(`${API_URL}/events/${evt.id}`);
+    const data = await res.json();
+    setImagePreview(data.image.formats.thumbnail.url);
+    setShowModal(false);
   };
 
   return (
@@ -165,7 +173,7 @@ export default function EditEventPage({ evt }) {
       </div>
 
       <Modal show={showModal} onClose={() => setShowModal(false)}>
-        IMAGE UPLOAD
+        <ImageUpload evtId={evt.id} imageUploaded={imageUploaded} />
       </Modal>
     </Layout>
   );
